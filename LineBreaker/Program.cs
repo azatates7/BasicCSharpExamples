09ee87194070df05e9077786be9231faa4a0b771
Azat Ateş <38978081+azatates7@users.noreply.github.com>
2020-07-22 00:16:23 +0300

    Add files via upload

---
+++ LineBreaker/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace LineBreaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "";
+            using (StreamReader sr = new StreamReader(@"C:\Users\azatates\Documents\old.txt"))
+            {
+                int i = 0;
+                do
+                {
+                    i++;
+                    string line = "," + sr.ReadLine();
+                    if (line != "")
+                    {
+                        text = text + line + Environment.NewLine;
+                    }
+                } while (sr.EndOfStream == false);
+            }
+            File.WriteAllText(@"C:\Users\azatates\Documents\new.txt", text);
+        }
+    }
+}
